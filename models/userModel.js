5fe3cb3ccc24ca50dde0b2df7d5ce29c8da74d53
Omerta85 <666Omerta@gmail.com>
2023-06-23 15:28:48 +0300

    add UserModel

--- models/userModel.js
+++ models/userModel.js
@@ -1,14 +1,49 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+    role:{
+        type:String,
+        required:[true,'role is required'],
+        enum:['admin', 'organization', 'user', 'hospital']
+    },
+    name:{
+        type:String,
+        required: function() {
+            return this.role === 'user' || this.role === 'admin';
+        }
+    },
+    organizationName:{
+        type:String,
+        required: function () {
+            return this.role === 'organization';
+        }
+    },
+    hospitalName:{
+        type:String,
+        required: function () {
+            return this.role === 'hospital';
+        }
+    },
     email:{
         type:String,
-        require:[true, 'email is required'],
+        required:[true, 'email is required'],
         unique:true
     },
     password:{
         type:String,
-        required:[],
-
+        required:[true, 'password is required']
+    },
+    website:{
+        type:String
+    },
+    address:{
+        type:String,
+        required:[true, 'address is required']
+    },
+    phone:{
+        type:String,
+        required:[true, 'phone number is required']
     }
-})
+}, {timestamps:true});
+
+module.exports = mongoose.model('users', userSchema)
