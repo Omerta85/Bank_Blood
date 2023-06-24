09156a39b2e104ace01de7eebba15a2d2fa134e6
Omerta85 <666Omerta@gmail.com>
2023-06-24 17:03:29 +0300

    add inventoryModel

---
+++ models/inventoryModel.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+
+const inventorySchema = new mongoose.Schema({
+    inventoryType:{
+        type:String,
+        required:[true,'Обов\'язково вкажіть тип'],
+        enum:['вхід', ' вихід']
+    },
+    bloodGroup:{
+        type:String,
+        required:[true,'Обов\'язково вкажіть групу крові'],
+        enum:['I(0+)', 'I(0-)', 'IV(AB+)', 'IV(AB-)', 'II(A+)', 'II(A-)', 'III(B+)','III(B-)']
+    },
+    quantity:{
+        type:Number,
+        required:[true,'Обов\'язково вкажіть кількість крові'],
+    },
+    organization:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'organizations',
+        required:[true, 'Обов\'язково вкажіть організацію']
+    },
+    hospital:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'users',
+        required: function (){
+            return this.inventoryType === "вихід";
+        }
+    },
+    donor:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'users',
+        required: function () {
+            return this.inventoryType === "вхід";
+        },
+    },
+},
+    { timestamps: true }
+)
+
+const inventoryModel = mongoose.model('Inventory', inventorySchema);
+
+export default inventoryModel;
